b7f424d3478cc8c51057ca85f1a35d6c884c8dee
yamsbot <devonbonderenka@gmail.com>
2024-08-13 00:36:55 -0400

    update

--- keys/sign.py
+++
@@ -1,7 +0,0 @@
-from tuf import *
-repository = repository_tool.load_repository("./")
-private_key = import_rsa_privatekey_from_file("./private.pem")
-repository.timestamp.load_signing_key(private_key)
-repository.mark_dirty(['timestamp'])
-repository.writeall()
-repository.write('timestamp')
